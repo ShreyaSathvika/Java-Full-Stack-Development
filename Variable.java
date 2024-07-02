5cbe3fca842e498daec7c8694d3dde4bb8e21c51
Shreya Mudettula <157104425+ShreyaSathvika@users.noreply.github.com>
2024-07-02 08:35:41 +0000

    Pending changes exported from your codespace

---
+++ Variable.java
@@ -0,0 +1,14 @@
+class Main
+{
+    public static void main(String[] args) {
+        var a = 99;
+        var b = 3.147;
+        var c = "Shreya";
+        var d = 'X';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
