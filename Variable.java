9d214c67ee117c8bdc86639d9e6aa2514c0ffc3f
Shreya Mudettula <157104425+ShreyaSathvika@users.noreply.github.com>
2024-07-02 10:33:13 +0000

    setup of github codespaces

---
+++ Variable.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        var a = 99;
+        var b = 3.147;
+        var c = "Shreya";
+        var d = 'X';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
